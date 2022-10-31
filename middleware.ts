d9cafcac8cfbd67e96dd7a3de03649d789ae008b
Cheslav Zhuravsky <happylolonly@gmail.com>
2022-10-31 20:21:30 +0400

    up

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,24 @@
+// import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
+// middleware being used for main.nabu.xyz protection
+// export const middleware = createNextAuthMiddleware({
+
+// });
+
+// export const config = {
+//   matcher: ["/(.*)", "/_next/static/chunks/(.*)"],
+// };
+
 import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
 
-// middleware being used for main.nabu.xyz protection
-export const middleware = createNextAuthMiddleware();
+export const middleware = createNextAuthMiddleware({
+  users: [
+    {
+      name: "123",
+      password: "1234",
+    },
+  ],
+});
 
 export const config = {
-  matcher: ["/(.*)", "/_next/static/chunks/(.*)"],
+  // matcher: ["/(.*)"], // Replace this with your own matcher logic
 };
